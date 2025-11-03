807d1a4ef7c905fc971766685e17461c5913f6f4
Duhoon <eclip6@naver.com>
2025-11-03 22:01:58 +0900

    feat: add Image Table DDL

--- apps/server/src/model/table.sql
+++ apps/server/src/model/table.sql
@@ -31,8 +31,8 @@ COMMENT ON TABLE categories.id IS 'DV:개발, MV:영화, MU: 음악, BO: 독서'
 -- 카테고리/작성글 맵핑 테이블
 CREATE TABLE post_category_links
 (
-  post_id INT NOT NULL,
-  category_id VARCHAR(5) NOT NULL,
+  post_id INT REFERENCES posts(id) NOT NULL,
+  category_id VARCHAR(5) REFERENCES categories(id) NOT NULL,
   is_active BOOLEAN DEFAULT true,
   updated_at TIMESTAMP DEFAULT NOW(),
   PRIMARY KEY (post_id, category_id)
@@ -44,7 +44,7 @@ COMMENT ON TABLE categories IS '카테고리/작성글 맵핑 테이블';
 CREATE TABLE tags
 (
   id SERIAL PRIMARY KEY,
-  name TEXT PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW()
 );
@@ -54,8 +54,23 @@ COMMENT ON TABLE tags IS '태그 테이블';
 -- 태그/작성글 맵핑 테이블
 CREATE TABLE tag_post_links
 (
-  tag_id INT NOT NULL,
-  post_id INT NOT NULL,
+  tag_id INT REFERENCES tags(id) NOT NULL,
+  post_id INT REFERENCES posts(id) NOT NULL,
   is_active INT NOT NULL,
   updated_at TIMESTAMP DEFAULT NOW() 
 );
+
+-- 이미지 테이블
+CREATE TABLE images
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  post_id INT REFERENCES posts(id) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  mimetype VARCHAR(50) NOT NULL,
+  uploaded_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW(),
+  width INT NOT NULL,
+  height INT NOT NULL,
+)
+
+COMMENT ON TABLE images IS '이미지 테이블'
