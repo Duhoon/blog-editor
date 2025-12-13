b0a56c13d93c1907f7b1752f69793ba706b3709f
Duhoon <eclip6@naver.com>
2025-12-13 20:32:05 +0900

    feat: Title & tag UI

--- apps/editor/src/components/Editor.tsx
+++ apps/editor/src/components/Editor.tsx
@@ -4,6 +4,9 @@ import "@toast-ui/editor/toastui-editor.css";
 import { useState, useRef, useEffect } from "react";
 
 export default function Editor() {
+  const [ title, setTitle ] = useState<string>("");
+  const [ tags, setTags ] = useState<string[]>([]);
+  const [ tag, setTag ] = useState<string>("");
   const [ content, setContent ] = useState<string>("");
   const editorRef = useRef<EditorUI>(null);
 
@@ -12,21 +15,75 @@ export default function Editor() {
   }, [content])
 
   return (
-    <EditorUI
-      previewStyle="vertical"
-      height="600px"
-      initialEditType="markdown"
-      onLoad={()=>{
-        
-      }}
-      onChange={()=>{
-        if (editorRef.current) {
-          const editorInstance = editorRef.current.getInstance();
-          const currentContent = editorInstance.getMarkdown();
-          setContent(currentContent);
-        }
-      }}
-      ref={editorRef}
-    />
+    <div className="px-4">
+      {/* 포스트 제목 입력 부분 */}
+      <div>
+        <input 
+          className={"w-full text-4xl p-2 border-1 border-gray-300 rounded-lg"} 
+          placeholder="제목을 입력하세요."
+          onChange={(e)=>{
+            setTitle(e.target.value);
+          }}
+          value={title}
+        />
+      </div>
+      {/* 포스트 태그 입력 부분 */}
+      <div className="py-2 flex gap-2 h-[50px]">
+          <input 
+            className={"bg-gray-200 rounded-xl outline-none px-2"}
+            placeholder="태그를 입력하세요."
+            value={tag}
+            onChange={(e)=>{
+              setTag(e.target.value);
+            }}
+            onKeyUp={(e)=>{
+              if (e.code === "Enter" && tag){
+                setTags((prevTags)=>[...prevTags, tag]);
+                setTag("");
+              }
+            }}
+          />
+          {
+            tags.map((tag, idx)=>{
+              return (
+                <div
+                  className={`bg-gray-400 rounded-xl
+                    h-full px-2 text-sm text-white outline-none flex items-center
+                    hover:cursor-pointer`
+                  }
+                  data-idx={idx} 
+                  key={`tag_${idx}`}
+                  onClick={(e)=>{
+                    const idx = Number.parseInt(e.currentTarget.dataset["idx"]!);
+                    setTags((before)=> [...before.slice(0, idx), ...before.slice(idx + 1)])
+                  }}
+                >
+                  {tag}
+                  <button
+                    className={`ml-2`}
+                  >X</button>
+                </div>
+              )
+            })
+          }
+      </div>
+      {/* 에디터, 포스트 내용 입력 부분 */}
+      <EditorUI
+        previewStyle="vertical"
+        height="90vh"
+        initialEditType="markdown"
+        onLoad={()=>{
+          
+        }}
+        onChange={()=>{
+          if (editorRef.current) {
+            const editorInstance = editorRef.current.getInstance();
+            const currentContent = editorInstance.getMarkdown();
+            setContent(currentContent);
+          }
+        }}
+        ref={editorRef}
+      />
+    </div>
   )
 }
